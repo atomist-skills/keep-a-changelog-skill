25cf7d1b79397e29f3298f550597b5cf373b5639
Christian Dupuis <cd@atomist.com>
2020-05-25 10:13:47 +0200

    Initial rework

---
+++ docs/long_description.md
@@ -0,0 +1,4 @@
+Automatically keep a changelog updated based on labels added 
+to issues, pull requests and commits.
+
+Based on https://keepachangelog.com.
---
+++ index.ts
@@ -0,0 +1,32 @@
+import {
+    ParameterType,
+    skill,
+} from "@atomist/skill/lib/skill";
+
+export const Skill = skill({
+
+    runtime: {
+        memory: 1024,
+        timeout: 60,
+    },
+
+    parameters: {
+        file: {
+            type: ParameterType.String,
+            displayName: "Name of changelog file",
+            description: "Relative path to the changelog file inside the repository (defaults to `CHANGELOG.md`)",
+            placeHolder: "CHANGELOG.md",
+            required: false,
+        },
+        repos: {
+            type: ParameterType.RepoFilter,
+            displayName: "Which repositories",
+            required: false,
+        }
+    },
+
+    subscriptions: [
+        "file://graphql/subscription/*.graphql",
+    ]
+
+});
